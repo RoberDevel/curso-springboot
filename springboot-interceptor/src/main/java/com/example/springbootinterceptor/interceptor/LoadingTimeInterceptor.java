b9b5d9b1dfb95de9556fceb02178087b673f2073
Roberto Bravo Barreiro <100875141+RoberDevel@users.noreply.github.com>
2024-04-10 12:57:52 +0200

    added total time to LoadingTimeInterceptor
    Se calcula el tiempo que pasa entre preHandle y postHandle

--- springboot-interceptor/src/main/java/com/example/springbootinterceptor/interceptor/LoadingTimeInterceptor.java
+++ springboot-interceptor/src/main/java/com/example/springbootinterceptor/interceptor/LoadingTimeInterceptor.java
@@ -1,9 +1,12 @@
 package com.example.springbootinterceptor.interceptor;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,16 +19,26 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-        log.info("LoadingTimeInterceptor: postHandle() saliendo");
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        log.info("LoadingTimeInterceptor: preHandle() entrando");
+
+        Long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        HandlerMethod controller = ((HandlerMethod) handler);
+        log.info("LoadingTimeInterceptor: preHandle() entrando.. " + controller.getMethod().getName());
+        Random random = new Random();
+        Integer delay = random.nextInt(1000);
+        Thread.sleep(delay);
         return true;
     }
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        Long endTime = System.currentTimeMillis();
+        Long startTime = (Long) request.getAttribute("startTime");
+        Long total = endTime - startTime;
+        log.info("LoadingTimeInterceptor: postHandle() saliendo.. " + ((HandlerMethod) handler).getMethod().getName()
+                + ", tiempo total: " + total + "ms");
+    }
 }
