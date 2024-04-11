dc853208632b0bf73f0dd2eecac09b5ff8c1ec99
Roberto Bravo Barreiro <100875141+RoberDevel@users.noreply.github.com>
2024-04-11 11:53:24 +0200

    imports cleaned

--- springboot-aop/src/main/java/com/example/springbootaop/aop/GreetingFooAspect.java
+++ springboot-aop/src/main/java/com/example/springbootaop/aop/GreetingFooAspect.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
--- springboot-aop/src/main/java/com/example/springbootaop/controller/GreetingController.java
+++ springboot-aop/src/main/java/com/example/springbootaop/controller/GreetingController.java
@@ -9,8 +9,7 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class GreetingController {
