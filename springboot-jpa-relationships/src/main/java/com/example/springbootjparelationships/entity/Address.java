2859d90b687b5ae09e647410f2387a48afc1d325
Roberto Bravo Barreiro <100875141+RoberDevel@users.noreply.github.com>
2024-04-17 12:25:09 +0200

    oneToMany

---
+++ springboot-jpa-relationships/src/main/java/com/example/springbootjparelationships/entity/Address.java
@@ -0,0 +1,29 @@
+package com.example.springbootjparelationships.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private Integer number;
+
+    public Address(String street, Integer number) {
+        this.street = street;
+        this.number = number;
+    }
+
+}
